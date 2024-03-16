7a261ce886bc5b32bdcb0bf0878d1b2592bf3643
RedPhoenixQ <steve.dark.03@gmail.com>
2024-03-17 00:38:44 +0100

    Fix tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,18 @@
-const { fontFamily } = require("tailwindcss/defaultTheme")
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: "media",
-  content: ["src/**/*.rs", "assets/components/**/*.js"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-  theme: {
+	darkMode: "media",
+	content: ["src/**/*.rs", "assets/components/**/*.js"],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+	theme: {
 		container: {
 			center: true,
 			padding: "2rem",
 			screens: {
-				"2xl": "1400px"
-			}
+				"2xl": "1400px",
+			},
 		},
 		extend: {
 			colors: {
@@ -25,42 +23,38 @@ module.exports = {
 				foreground: "hsl(var(--foreground) / <alpha-value>)",
 				primary: {
 					DEFAULT: "hsl(var(--primary) / <alpha-value>)",
-					foreground: "hsl(var(--primary-foreground) / <alpha-value>)"
+					foreground: "hsl(var(--primary-foreground) / <alpha-value>)",
 				},
 				secondary: {
 					DEFAULT: "hsl(var(--secondary) / <alpha-value>)",
-					foreground: "hsl(var(--secondary-foreground) / <alpha-value>)"
+					foreground: "hsl(var(--secondary-foreground) / <alpha-value>)",
 				},
 				destructive: {
 					DEFAULT: "hsl(var(--destructive) / <alpha-value>)",
-					foreground: "hsl(var(--destructive-foreground) / <alpha-value>)"
+					foreground: "hsl(var(--destructive-foreground) / <alpha-value>)",
 				},
 				muted: {
 					DEFAULT: "hsl(var(--muted) / <alpha-value>)",
-					foreground: "hsl(var(--muted-foreground) / <alpha-value>)"
+					foreground: "hsl(var(--muted-foreground) / <alpha-value>)",
 				},
 				accent: {
 					DEFAULT: "hsl(var(--accent) / <alpha-value>)",
-					foreground: "hsl(var(--accent-foreground) / <alpha-value>)"
+					foreground: "hsl(var(--accent-foreground) / <alpha-value>)",
 				},
 				popover: {
 					DEFAULT: "hsl(var(--popover) / <alpha-value>)",
-					foreground: "hsl(var(--popover-foreground) / <alpha-value>)"
+					foreground: "hsl(var(--popover-foreground) / <alpha-value>)",
 				},
 				card: {
 					DEFAULT: "hsl(var(--card) / <alpha-value>)",
-					foreground: "hsl(var(--card-foreground) / <alpha-value>)"
-				}
+					foreground: "hsl(var(--card-foreground) / <alpha-value>)",
+				},
 			},
 			borderRadius: {
 				lg: "var(--radius)",
 				md: "calc(var(--radius) - 2px)",
-				sm: "calc(var(--radius) - 4px)"
+				sm: "calc(var(--radius) - 4px)",
 			},
-			fontFamily: {
-				sans: [...fontFamily.sans]
-			}
-		}
-  },
-}
-
+		},
+	},
+};
